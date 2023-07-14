c565ff134b391e32bd7e7f0e9abaca1ab3ce64e9
afroze9 <afrozeamjad@gmail.com>
2023-07-14 11:15:39 +0500

    Init

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+	let currentTile: number = 0;
+</script>
+
+<AppRail>
+	<AppRailAnchor href="/companies">
+		<svelte:fragment slot="lead">
+			<i class="fa-solid fa-building text-2xl" />
+		</svelte:fragment>
+		<span>Companies</span>
+	</AppRailAnchor>
+	<AppRailAnchor href="/projects">
+		<svelte:fragment slot="lead">
+			<i class="fa-solid fa-diagram-project text-2xl" />
+		</svelte:fragment>
+		<span>Projects</span>
+	</AppRailAnchor>
+</AppRail>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Nexus</strong>
+			</svelte:fragment>
+
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+				<a class="btn btn-sm variant-ghost-surface" href="/"> Login </a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Sidebar />
+	</svelte:fragment>
+	<slot />
+</AppShell>
